16bda74fa84e10eeebb1d88fc182ac082a7ece5c
Eric-does <contact@steadygrowthstudios.com>
2024-11-15 11:55:47 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,21 @@
+import { Inter } from 'next/font/google'
 import './globals.css'
+import { Toaster } from 'sonner'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Ideas App',
+  description: 'Submit and vote on ideas',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   )
 }
